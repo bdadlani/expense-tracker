07add898e89ef14eecde48d6019273c702397903
bdadlani <bdadlani17@gmail.com>
2024-11-11 20:26:10 +0530

    deleted:    src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-package in.ac.adit.pwj.miniproject.expenses;
-
-public class Main {
-    public static void main(String[] args) {
-        new ExpenseTracker();  // Create and display the GUI
-    }
-}
-
-
